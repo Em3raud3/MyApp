200c7e4552ee2401a5fd0045c4e209b806aec3e5
Weng, Jeffrey <Jeffrey.Weng@wv.gov>
2026-05-04 11:43:12 -0400

    Add PDF & Document Remediation page and menu link
    Added a new PDF & Document Remediation page with detailed ADA compliance guidance, including archiving criteria and "Current Use" definitions. Updated AdaController and navigation menu to support the new page. Also removed extra whitespace and the footer from _Layout.cshtml.

--- Controllers/AdaController.cs
+++ Controllers/AdaController.cs
@@ -5,5 +5,6 @@ namespace MyApp.Controllers
     public class AdaController : Controller
     {
         public IActionResult ADA_Compliance() => View();
+        public IActionResult PDF_Remediation() => View();
     }
 }
